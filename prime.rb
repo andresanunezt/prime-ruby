da827684615acfcf2e095834ad5f3b1a1edd77e4
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-01 13:31:27 -0500

    commit tree e6863aca5e88014e3cdc55ec27eb5b589c0a1101

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+         false
+    else
+       
+    # check all the numbers between  between 2 and the number minus one and send them to an array and loops through all of them to checl to see if they are possible factors
+        (2..num-1).to_a.all? do |factor|
+            num % factor != 0
+        end
+    end
+end
+
+
+
+# modulo operator - checks remainders 
+
